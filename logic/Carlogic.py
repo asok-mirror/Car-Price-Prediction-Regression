57d3c06896a2d3cdb57cdf400dcb07b1f5b3a56a
asokkalidass kalisamy <asok.kalidass@gmail.com>
2021-06-09 15:27:18 -0700

    Removed toggle nav for mobile and removed hard coded predict value

--- logic/Carlogic.py
+++ logic/Carlogic.py
@@ -21,16 +21,16 @@ class CarLogic:
 
     def predictPrice(self, car: car.CarDto):
 
-        # x_test = pd.DataFrame([[car.carModel, car.year, car.kmDriven, car.fuel, car.sellerType, car.transmission,
-        #                         car.owner, car.mileage, car.engine, car.maxPower, car.torque, car.seats]],
-        #                       columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner',
-        #                                'mileage',
-        #                                'engine', 'max_power', 'torque', 'seats'], dtype=object)
-        x_test = pd.DataFrame([['Toyota Innova Crysta 2.5 VX BS IV', 2011, 60000.0, 'Diesel', 'Individual', 'Manual',
-                                'First Owner', 12.8, 2494.0, 102.0, 20.0, 7.0]],
+        x_test = pd.DataFrame([[car.carModel, car.year, car.kmDriven, car.fuel, car.sellerType, car.transmission,
+                                car.owner, car.mileage, car.engine, car.maxPower, car.torque, car.seats]],
                               columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner',
                                        'mileage',
                                        'engine', 'max_power', 'torque', 'seats'], dtype=object)
+        # x_test = pd.DataFrame([['Toyota Innova Crysta 2.5 VX BS IV', 2011, 60000.0, 'Diesel', 'Individual', 'Manual',
+        #                         'First Owner', 12.8, 2494.0, 102.0, 20.0, 7.0]],
+        #                       columns=['name', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner',
+        #                                'mileage',
+        #                                'engine', 'max_power', 'torque', 'seats'], dtype=object)
         try:
             result = CarLogic.model.predict(x_test)[0]
             return "Predicted car price is Rs. <b>" + str(result) + "</b>"
